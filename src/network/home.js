c934c5ae1601839036feba6eb7981cb704b892fc
杨文涛 <1545052914@qq.com>
2021-11-15 12:49:09 +0800

    还来

--- src/network/home.js
+++ src/network/home.js
@@ -1,8 +1,8 @@
 const $ = require('jquery')
 export function getHomeMultdata() {
-    return $.getJSON('//123.207.32.32:8000/home/multidata')
+    return $.getJSON('http://123.207.32.32:8000/home/multidata')
 }
 export function getHomegoods(type,page) {
-    return $.getJSON('//list.mogu.com/search?callback=?&sort='+type+'&page='+page)
+    return $.getJSON('https://list.mogu.com/search?callback=?&sort='+type+'&page='+page)
 }
     
