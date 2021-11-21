7cd754c37254543f5ab7c3b368c098e3c99eff83
杨文涛 <1545052914@qq.com>
2021-11-21 14:16:45 +0800

    提交

--- src/components/content/views/Home/Home.vue
+++ src/components/content/views/Home/Home.vue
@@ -5,7 +5,6 @@
             <template #center>购物车</template>
             <template #right></template>
         </nav-bar>
-        <tab-contro v-show="tabControIsShow" ref="tabContro1" :title="['综合','销量','上新']" :sorts="sorts" @tabContro="tabContro"/>
         <scroll ref="scroll" 
         @upload='upload' 
         @block='block'
